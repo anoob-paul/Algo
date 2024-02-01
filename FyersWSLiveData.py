d68f0c76f497b40e730714a8c1a935c99bd907f9
Anoob Paul <155704736+anoobpaul@users.noreply.github.com>
2024-02-01 12:23:30 +0530

    Update FyersWSLiveData.py
    due to a space between the client id and save token were causing some issues ands the live data were not printing . took  a lot of time to debug this 
    
    ws_access_token = f"{client_id}:{saved_token}"   # make sure there is no space between them :)

--- FyersWSLiveData.py
+++ FyersWSLiveData.py
@@ -3,10 +3,10 @@ from fyers_apiv3 import fyersModel
 from fyers_apiv3.FyersWebsocket import data_ws
 from datetime import datetime
 from tabulate import tabulate
-import os
+import os,  json
 import pandas as pd
 
-
+global fyers
 # reading from config file 
 def read_config(file_path):
     config = {}
@@ -16,41 +16,23 @@ def read_config(file_path):
             config[key.strip()] = value.strip()
     return config
 
-config_file_path = 'config.txt'
+config_file_path = 'config.ini'
 config = read_config(config_file_path)
 
 # Access the values by attribute name
-
-global fyers
 client_id = config['client_id']  #client id and secret key are fetched from config file 
-secret_key = config['secret_key']  
+secret_key = config['secret_key']
+print("Client ID : ",client_id)
+print("Secreat Key : ",secret_key)
 redirect_uri = "https://www.google.co.in/" 
 response_type = "code"  #  This value must always be “code”
 grant_type = "authorization_code"  
 
 
 
-
-def writeAccessCodeToFile(token):
-    with open('authcode.txt', 'w') as file:
-        file.write(token)
-
-
-def readAccessCodeFromFile():
-    try:
-        with open('authcode.txt', 'r') as file:
-            return file.read().strip()
-    except FileNotFoundError:
-        return None
-
-saved_token = readAccessCodeFromFile()
-
-
-
 def generateAccessToken() :
-
-    response_type = "code"  # This value must always be “code”
-    grant_type = "authorization_code"  
+    # response_type = "code"  # This value must always be “code”
+    # grant_type = "authorization_code"  
 
     # Create a session model with the provided credentials
     session = fyersModel.SessionModel(
@@ -68,65 +50,67 @@ def generateAccessToken() :
     # Generate the access token using the authorization code
     auth_response = session.generate_token()
     access_token = auth_response['access_token']
-    print(' access token is :',access_token)
+    print(' access token newly generated is =============== :',access_token)
 
     return access_token
 
+
+def writeAccessCodeToFile(token):
+    with open('authcode.ini', 'w') as file:
+        file.write(token)
+
+def readAccessCodeFromFile():
+    try:
+        with open('authcode.ini', 'r') as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        return None
+    
+saved_token = readAccessCodeFromFile()
+print('Saved token is +++++++++++++++++++:',saved_token)
 
 if saved_token:
     print('Token loaded from file')
     fyers = fyersModel.FyersModel(client_id=client_id, is_async=False, token=saved_token, log_path="")
 
 else:
+    print('access token not found. so generating a new access token')
     access_token = generateAccessToken()
     writeAccessCodeToFile(access_token)
 
-profile = fyers.get_profile()
-# funds = fyers.funds()
-# print(profile)
-# print(funds)
-
-
-
-
 def onmessage(message):
-    print("Response:", message)
-
+    print("Received Message:", message)
 
 def onerror(message):
     print("Error:", message)
 
-
 def onclose(message):
     print("Connection closed:", message)
-
 
 def onopen():
     # Specify the data type and symbols you want to subscribe to
     data_type = "SymbolUpdate"
-
     # Subscribe to the specified symbols and data type
-    symbols = ["NSE:WTICRUDE24FEBFUT"]
+    symbols = ["NSE:NIFTY50-INDEX"]
     ws_fyers.subscribe(symbols=symbols, data_type=data_type)
-
     # Keep the socket running to receive real-time data
     ws_fyers.keep_running()
 
 # Replace the sample access token with your actual access token obtained from Fyers
-ws_access_token = f"{client_id} : {saved_token}"
-
+ws_access_token = f"{client_id}:{saved_token}"
+print('ws_access_token is ++++++++++++++++:',ws_access_token)
 # Create a FyersDataSocket instance with the provided parameters
 ws_fyers = data_ws.FyersDataSocket(
     access_token=ws_access_token,       # Access token in the format "appid:accesstoken"
     log_path="",                     # Path to save logs. Leave empty to auto-create logs in the current directory.
-    litemode=True,                  # Lite mode disabled. Set to True if you want a lite response.
-    write_to_file=False,              # Save response in a log file instead of printing it.
+    litemode=False,                  # Lite mode disabled. Set to True if you want a lite response.
+    write_to_file=False,             # Save response in a log file instead of printing it.
     reconnect=True,                  # Enable auto-reconnection to WebSocket on disconnection.
     on_connect=onopen,               # Callback function to subscribe to data upon connection.
     on_close=onclose,                # Callback function to handle WebSocket connection close events.
     on_error=onerror,                # Callback function to handle WebSocket errors.
     on_message=onmessage             # Callback function to handle incoming messages from the WebSocket.
 )
-
 # Establish a connection to the Fyers WebSocket
 ws_fyers.connect()
+
